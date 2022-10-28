8653066a4aaed21991dfde7c83cde3b69b9a54eb
LeeYooBin <santosroberdan25@gmail.com>
2022-10-28 09:35:01 -0300

    Creating LoginInput stories

---
+++ stories/LoginInput.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { LoginInput } from '../src/components/LoginInput';
+
+export default {
+    title: 'Components/LoginInput',
+    component: LoginInput
+} as ComponentMeta<typeof LoginInput>
+
+const Template: ComponentStory<typeof LoginInput> = args => <LoginInput {...args}/>;
+
+export const Primary = Template.bind({});
