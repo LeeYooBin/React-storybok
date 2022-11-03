286051c29e796d6ae22448d9a9da379faf2eea6e
LeeYooBin <santosroberdan25@gmail.com>
2022-11-03 17:38:03 -0300

    Fixing style of the InputForm component and passing props to it

--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -1,46 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Content = styled.div`
-  width: 100%;
-  height: auto
-  padding: 20px;
+const Content = styled.div<InputForm>`
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+  font-family: 'Open Sans', sans-serif;
+  position: relative;
+  width: 100vw;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 20px 30px;
 `;
 
 const Label = styled.label`
-  width: 100%;
-  font-size: 20px;
-  font-weight: 400;
-  margin-bottom: 7px;
+  margin-bottom: 1.5vh;
+  font-size: 1rem;
+  opacity: 0.7;
 `;
 
 const Input = styled.input`
+  border: none;
+  outline: none;
+  color: ${({ color }: InputForm) => color ? color : '#000'};
+  font-size: 1rem;
+  font-weight: 600;
   width: 100%;
-  height: 25px;
-  border: 0;
-  outline: 0;
-  font-size: 14px;
-  color: #7640d0;
-  font-weight: 550;
 
-  ::-webkit-input-placeholder {
-    color: #393335;
-    opacity: 50%;
-    font-size: 14px;
+  ::placeholder {
+    opacity: 0.35;
+    font-style: italic;
+    font-size: 0.9rem;
     font-weight: 400;
   }
 `;
 
 export interface InputForm {
-  label?: string;
-  name?: string;
-  value?: string;
+  title?: string;
   placeholder?: string;
+  color?: string;
 }
 
-export const InputForm = ({ label, name, value, placeholder }: InputForm) => (
+export const InputForm = ({ title='Título', placeholder='Placeholder', color} : InputForm) => (
   <Content>
-    <Label>{label}</Label>
-    <Input name={name} value={value} type="text" placeholder={placeholder} />
+    <Label>{title}</Label>
+    <Input placeholder={`“${placeholder}”`} color={color}/>
   </Content>
 );
