269e0af704924e08840683a41ecbc95a30c149f1
LeeYooBin <santosroberdan25@gmail.com>
2022-11-04 10:42:34 -0300

    Creating and styling SaveButton component

---
+++ src/components/SaveButton/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const Button = styled.button<SaveButton>`
+  padding: 6px 12px;
+  border: solid 2.5px ${({ color }: SaveButton) => color ? color : "#000"};
+  border-radius: 5px;
+  background-color: #fff;
+  color: ${({ color }: SaveButton) => color ? color : "#000"};
+  font-weight: 600;
+  opacity: 0.9;
+  cursor: pointer;
+`;
+
+export interface SaveButton {
+  text?: string;
+  color?: string;
+}
+
+export const SaveButton = ({ text = 'Text', color }: SaveButton) => (
+  <Button color={color}>
+    {text}
+  </Button>
+);
